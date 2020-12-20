17048af1d860df1217174b6eb25c28bcba51053e
Huang ChenGang <hcggang@163.com>
2020-12-20 15:07:20 +0800

    新建项目

---
+++ src/main/java/com/jn/xingdaba/gateway/JnXingdabaGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.jn.xingdaba.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JnXingdabaGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JnXingdabaGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jn/xingdaba/gateway/JnXingdabaGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jn.xingdaba.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JnXingdabaGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
